b7a4322e98745d05465041ce973598bd6602cda9
Martin Edström <meedstrom91@gmail.com>
2024-07-24 01:33:31 +0200

    Rename

--- dropped-olivetti.el
+++ dropped-olivetti.el
@@ -1,4 +1,4 @@
-;;; olivetti.el --- Minor mode for a nice writing environment -*- lexical-binding: t; -*-
+;;; dropped-olivetti.el --- Minor mode for a nice writing environment -*- lexical-binding: t; -*-
 
 ;; Copyright (c) 2014-2023  Paul W. Rankin
 
@@ -6,7 +6,7 @@
 ;; Keywords: wp, text
 ;; Version: 2.0.5
 ;; Package-Requires: ((emacs "24.4"))
-;; URL: https://github.com/rnkn/olivetti
+;; URL: https://github.com/rnkn/dropped-olivetti
 
 ;; This file is not part of GNU Emacs.
 
@@ -25,7 +25,7 @@
 
 ;;; Commentary:
 
-;; Olivetti
+;; Dropped-Olivetti
 ;; ========
 
 ;; A simple Emacs minor mode for a nice writing environment.
@@ -39,19 +39,19 @@
 ;;    the window width (a float between 0.0 and 1.0), or nil which uses the value
 ;;    of fill-column +2.
 ;;  - Interactively change body width with:
-;;    olivetti-shrink C-c { { { ...
-;;    olivetti-expand C-c } } } ...
-;;    olivetti-set-width C-c |
-;;  - If olivetti-body-width is an integer, the text body width will
+;;    dropped-olivetti-shrink C-c { { { ...
+;;    dropped-olivetti-expand C-c } } } ...
+;;    dropped-olivetti-set-width C-c |
+;;  - If dropped-olivetti-body-width is an integer, the text body width will
 ;;    scale with use of text-scale-mode, whereas if a fraction (float) then
 ;;    the text body width will remain at that fraction.
-;;  - Change the way the text body margins look with option olivetti-style: use
+;;  - Change the way the text body margins look with option dropped-olivetti-style: use
 ;;    margins, fringes, or both for a fancy "page" look.
-;;  - Customize olivetti-fringe face to affect only Olivetti buffers.
+;;  - Customize dropped-olivetti-fringe face to affect only Dropped-Olivetti buffers.
 ;;  - Optionally remember the state of visual-line-mode on entry and
 ;;    recall its state on exit.
 
-;; Olivetti keeps everything it does buffer-local, so you can write prose
+;; Dropped-Olivetti keeps everything it does buffer-local, so you can write prose
 ;; in one buffer and code in another, side-by-side in the same frame.
 
 
@@ -64,7 +64,7 @@
 ;; Installation
 ;; ------------
 
-;; The latest stable release of Olivetti is available via
+;; The latest stable release of Dropped-Olivetti is available via
 ;; [MELPA-stable][1]. First, add MELPA-stable to your package archives:
 
 ;;     M-x customize-option RET package-archives RET
@@ -72,7 +72,7 @@
 ;; Insert an entry named melpa-stable with URL:
 ;; https://stable.melpa.org/packages/
 
-;; You can then find the latest stable version of olivetti in the
+;; You can then find the latest stable version of dropped-olivetti in the
 ;; list returned by:
 
 ;;     M-x list-packages RET
@@ -87,12 +87,12 @@
 ;; Download the latest tagged release, move this file into your load-path
 ;; and add to your init.el file:
 
-;;     (require 'olivetti)
+;;     (require 'dropped-olivetti)
 
-;; If you wish to contribute to or alter Olivetti's code, clone the
+;; If you wish to contribute to or alter Dropped-Olivetti's code, clone the
 ;; repository into your load-path and require as above:
 
-;;     git clone https://github.com/rnkn/olivetti.git
+;;     git clone https://github.com/rnkn/dropped-olivetti.git
 
 
 ;; Bugs and Feature Requests
@@ -101,7 +101,7 @@
 ;; Use GitHub issues or send me an email (address in the package header).
 ;; For bugs, please ensure you can reproduce with:
 
-;;     $ emacs -Q -l olivetti.el
+;;     $ emacs -Q -l dropped-olivetti.el
 
 
 ;; Hints
@@ -110,7 +110,7 @@
 ;; To always use a different width for a specific file, set a File
 ;; Variable:
 
-;;     M-x add-file-local-variable RET olivetti-body-width RET 66 RET
+;;     M-x add-file-local-variable RET dropped-olivetti-body-width RET 66 RET
 
 ;; See (info "(emacs) File Variables")
 
@@ -122,16 +122,16 @@
 ;; larger scope, I recommend [Writeroom Mode](https://github.com/joostkremers/writeroom-mode).
 
 
-;; [1]: https://stable.melpa.org/#/olivetti
-;; [2]: https://melpa.org/#/olivetti
+;; [1]: https://stable.melpa.org/#/dropped-olivetti
+;; [2]: https://melpa.org/#/dropped-olivetti
 
 ;;; Code:
 
 (require 'fringe)
 
-(defgroup olivetti ()
+(defgroup dropped-olivetti ()
   "Minor mode for a nice writing environment."
-  :prefix "olivetti-"
+  :prefix "dropped-olivetti-"
   :group 'text)
 
 
@@ -139,21 +139,21 @@
 
 (eval-when-compile
   (require 'lisp-mnt)
-  (defconst olivetti-version
+  (defconst dropped-olivetti-version
     (lm-version load-file-name)))
 
-(defvar olivetti--split-window-preferred-function
+(defvar dropped-olivetti--split-window-preferred-function
   nil
   "Value of `split-window-preferred-function' at initialization.")
 
-(defvar olivetti--face-remap
+(defvar dropped-olivetti--face-remap
   nil
   "Saved cookie from `face-remap-add-relative' at initialization.")
 
 
 ;;; Options ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defcustom olivetti-body-width
+(defcustom dropped-olivetti-body-width
   nil
   "Text body width to which to adjust relative margin width.
 If an integer, set text body width to that integer in columns; if
@@ -174,30 +174,30 @@ This option does not affect file contents."
                  (float 0.5))
   :safe (lambda (value)
           (or (numberp value) (null value))))
-;; (make-variable-buffer-local 'olivetti-body-width)
+;; (make-variable-buffer-local 'dropped-olivetti-body-width)
 
-(defcustom olivetti-minimum-body-width
+(defcustom dropped-olivetti-minimum-body-width
   40
   "Minimum width in columns of text body."
   :type 'integer
   :safe 'integerp)
 
-(defcustom olivetti-lighter
+(defcustom dropped-olivetti-lighter
   " Olv"
-  "Mode-line indicator for `olivetti-mode'."
+  "Mode-line indicator for `dropped-olivetti-mode'."
   :type '(choice (const :tag "No lighter" "") string)
   :safe 'stringp)
 
-(defcustom olivetti-recall-visual-line-mode-entry-state
+(defcustom dropped-olivetti-recall-visual-line-mode-entry-state
   t
   "Recall the state of `visual-line-mode' upon exiting.
 When non-nil, remember if `visual-line-mode' was enabled or not
-upon activating `olivetti-mode' and restore that state upon
+upon activating `dropped-olivetti-mode' and restore that state upon
 exiting."
   :type 'boolean
   :safe 'booleanp)
 
-(defcustom olivetti-style
+(defcustom dropped-olivetti-style
   nil
   "Window elements used to balance the text body.
 Valid options are:
@@ -213,30 +213,30 @@ will fall back to margins on console."
                  (const :tag "Fringes and Margins" fancy))
   :set (lambda (symbol value)
          (set-default symbol value)
-         (when (featurep 'olivetti)
-           (olivetti-reset-all-windows))))
+         (when (featurep 'dropped-olivetti)
+           (dropped-olivetti-reset-all-windows))))
 
-(defcustom olivetti-margin-width
+(defcustom dropped-olivetti-margin-width
   10
   "Width in columns of margin between text body and fringes.
-Only has any effect when `olivetti-style' is set to `fancy'."
+Only has any effect when `dropped-olivetti-style' is set to `fancy'."
   :type '(choice (const :tag "None" nil)
                  (integer :tag "Columns" 10))
   :safe 'integerp
   :set (lambda (symbol value)
          (set-default symbol value)
-         (when (featurep 'olivetti)
-           (olivetti-reset-all-windows))))
+         (when (featurep 'dropped-olivetti)
+           (dropped-olivetti-reset-all-windows))))
 
-(defface olivetti-fringe
+(defface dropped-olivetti-fringe
   '((t (:inherit fringe)))
-  "Face for the fringes when `olivetti-style' is non-nil."
-  :group 'olivetti)
+  "Face for the fringes when `dropped-olivetti-style' is non-nil."
+  :group 'dropped-olivetti)
 
 
 ;;; Set Windows ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun olivetti-scale-width (width)
+(defun dropped-olivetti-scale-width (width)
   "Scale WIDTH in accordance with the face height.
 For compatibility with `text-scale-mode', if
 `face-remapping-alist' includes a :height property on the default
@@ -247,73 +247,73 @@ if it is an integer, and otherwise return WIDTH."
       (setq height (/ height 100.0)))
     (round (* width (or height 1)))))
 
-(defun olivetti-normalize-width (width window)
-  "Parse WIDTH to a safe pixel value for `olivetti-body-width' for WINDOW."
+(defun dropped-olivetti-normalize-width (width window)
+  "Parse WIDTH to a safe pixel value for `dropped-olivetti-body-width' for WINDOW."
   (let ((char-width (frame-char-width (window-frame window)))
         (window-width-pix (window-body-width window t))
         min-width-pix)
     (setq min-width-pix (* char-width
-                           (+ olivetti-minimum-body-width
-                              (% olivetti-minimum-body-width 2))))
-    (olivetti-scale-width
+                           (+ dropped-olivetti-minimum-body-width
+                              (% dropped-olivetti-minimum-body-width 2))))
+    (dropped-olivetti-scale-width
      (if (floatp width)
          (floor (max min-width-pix (* window-width-pix (min width 1.0))))
        (max min-width-pix (min (* width char-width) window-width-pix))))))
 
-(defun olivetti-reset-window (window)
-  "Remove Olivetti's parameters and margins from WINDOW."
-  (when (eq (window-parameter window 'split-window) 'olivetti-split-window)
+(defun dropped-olivetti-reset-window (window)
+  "Remove Dropped-Olivetti's parameters and margins from WINDOW."
+  (when (eq (window-parameter window 'split-window) 'dropped-olivetti-split-window)
     (set-window-parameter window 'split-window nil))
   (if (consp fringe-mode)
       (set-window-fringes window (car fringe-mode) (cdr fringe-mode))
     (set-window-fringes window fringe-mode fringe-mode))
   (set-window-margins window nil))
 
-(defun olivetti-reset-all-windows ()
-  "Call `olivetti-reset-window' on all windows."
-  (walk-windows #'olivetti-reset-window nil t))
+(defun dropped-olivetti-reset-all-windows ()
+  "Call `dropped-olivetti-reset-window' on all windows."
+  (walk-windows #'dropped-olivetti-reset-window nil t))
 
-(defun olivetti-split-window (&optional window size side pixelwise)
+(defun dropped-olivetti-split-window (&optional window size side pixelwise)
   "Call `split-window' after resetting WINDOW.
 Pass SIZE, SIDE and PIXELWISE unchanged."
-  (olivetti-reset-all-windows)
+  (dropped-olivetti-reset-all-windows)
   (split-window window size side pixelwise))
 
-(defun olivetti-split-window-sensibly (&optional window)
-  "Like `olivetti-split-window' but call `split-window-sensibly'.
+(defun dropped-olivetti-split-window-sensibly (&optional window)
+  "Like `dropped-olivetti-split-window' but call `split-window-sensibly'.
 Pass WINDOW unchanged."
-  (olivetti-reset-all-windows)
-  (funcall olivetti--split-window-preferred-function window))
+  (dropped-olivetti-reset-all-windows)
+  (funcall dropped-olivetti--split-window-preferred-function window))
 
-(defun olivetti-set-window (window-or-frame)
+(defun dropped-olivetti-set-window (window-or-frame)
   "Balance window margins displaying current buffer.
 If WINDOW-OR-FRAME is a frame, cycle through windows displaying
 current buffer in that frame, otherwise only work on the selected
 window."
   (if (framep window-or-frame)
       (with-selected-frame window-or-frame
-        (walk-windows #'olivetti-set-window))
+        (walk-windows #'dropped-olivetti-set-window))
     (with-selected-window window-or-frame
       (unless (minibufferp)
-        (olivetti-reset-window window-or-frame)
-        ;; If `olivetti-body-width' is nil, we need to calculate from
+        (dropped-olivetti-reset-window window-or-frame)
+        ;; If `dropped-olivetti-body-width' is nil, we need to calculate from
         ;; `fill-column'
-        (when (null olivetti-body-width)
-          (setq olivetti-body-width (+ fill-column 2)))
+        (when (null dropped-olivetti-body-width)
+          (setq dropped-olivetti-body-width (+ fill-column 2)))
         (let ((char-width-pix   (frame-char-width (window-frame window-or-frame)))
               (window-width-pix (window-body-width window-or-frame t))
-              (safe-width-pix   (olivetti-normalize-width
-                                 olivetti-body-width window-or-frame)))
+              (safe-width-pix   (dropped-olivetti-normalize-width
+                                 dropped-olivetti-body-width window-or-frame)))
           ;; Handle possible display of fringes
-          (when (and window-system olivetti-style)
+          (when (and window-system dropped-olivetti-style)
             (let ((fringe-total (- (window-pixel-width window-or-frame)
                                    safe-width-pix))
                   fringe)
               ;; Account for fancy display
-              (when (eq olivetti-style 'fancy)
+              (when (eq dropped-olivetti-style 'fancy)
                 (setq fringe-total
                       (- fringe-total
-                         (* olivetti-margin-width char-width-pix 2))))
+                         (* dropped-olivetti-margin-width char-width-pix 2))))
               ;; Calculate a single fringe width
               (setq fringe (max (round (/ fringe-total 2.0)) 0))
               ;; Set the fringes
@@ -335,13 +335,13 @@ window."
             (set-window-margins window-or-frame left-margin right-margin)))
         ;; Set remaining window parameters
         (set-window-parameter window-or-frame 'split-window
-                              'olivetti-split-window)))))
+                              'dropped-olivetti-split-window)))))
 
-(defun olivetti-set-buffer-windows ()
+(defun dropped-olivetti-set-buffer-windows ()
   "Balance window margins in all windows displaying current buffer.
 Cycle through all windows in all visible frames displaying the
-current buffer, and call `olivetti-set-window'."
-  (mapc #'olivetti-set-window (get-buffer-window-list nil nil 'visible)))
+current buffer, and call `dropped-olivetti-set-window'."
+  (mapc #'dropped-olivetti-set-window (get-buffer-window-list nil nil 'visible)))
 
 
 
@@ -350,47 +350,47 @@ current buffer, and call `olivetti-set-window'."
 (require 'face-remap)
 
 ;;;###autoload
-(define-minor-mode olivetti-mode
-  "Olivetti provides a nice writing environment.
+(define-minor-mode dropped-olivetti-mode
+  "Dropped-Olivetti provides a nice writing environment.
 Window margins are set to relative widths to accomodate a text
-body width set with `olivetti-body-width'."
+body width set with `dropped-olivetti-body-width'."
   :global t
-  (if olivetti-mode
+  (if dropped-olivetti-mode
       (progn
         (add-hook 'window-size-change-functions
-                  #'olivetti-set-window t)
+                  #'dropped-olivetti-set-window t)
         ;; (add-hook 'change-major-mode-hook
-        ;; #'olivetti-reset-all-windows nil)
+        ;; #'dropped-olivetti-reset-all-windows nil)
         (add-hook 'text-scale-mode-hook
-                  #'olivetti-set-buffer-windows t)
-        (unless (bound-and-true-p olivetti--split-window-preferred-function)
-          (setq olivetti--split-window-preferred-function
+                  #'dropped-olivetti-set-buffer-windows t)
+        (unless (bound-and-true-p dropped-olivetti--split-window-preferred-function)
+          (setq dropped-olivetti--split-window-preferred-function
                 split-window-preferred-function))
         (setq split-window-preferred-function
-              #'olivetti-split-window-sensibly)
-        (setq olivetti--face-remap
-              (face-remap-add-relative 'fringe 'olivetti-fringe))
-        (walk-windows #'olivetti-set-window nil t))
+              #'dropped-olivetti-split-window-sensibly)
+        (setq dropped-olivetti--face-remap
+              (face-remap-add-relative 'fringe 'dropped-olivetti-fringe))
+        (walk-windows #'dropped-olivetti-set-window nil t))
     (remove-hook 'window-configuration-change-hook
-                 #'olivetti-set-buffer-windows )
+                 #'dropped-olivetti-set-buffer-windows )
     (remove-hook 'window-size-change-functions
-                 #'olivetti-set-window )
+                 #'dropped-olivetti-set-window )
     (remove-hook 'text-scale-mode-hook
-                 #'olivetti-set-window )
-    (olivetti-set-buffer-windows)
+                 #'dropped-olivetti-set-window )
+    (dropped-olivetti-set-buffer-windows)
     (set-window-margins nil left-margin-width right-margin-width)
     (setq split-window-preferred-function
-          olivetti--split-window-preferred-function)
-    (when olivetti--face-remap
-      (face-remap-remove-relative olivetti--face-remap))
-    (mapc #'kill-local-variable '(olivetti--face-remap
-                                  olivetti--split-window-preferred-function))))
+          dropped-olivetti--split-window-preferred-function)
+    (when dropped-olivetti--face-remap
+      (face-remap-remove-relative dropped-olivetti--face-remap))
+    (mapc #'kill-local-variable '(dropped-olivetti--face-remap
+                                  dropped-olivetti--split-window-preferred-function))))
 
 
 
-(provide 'olivetti)
+(provide 'dropped-olivetti)
 
-;;; olivetti.el ends here
+;;; dropped-olivetti.el ends here
 
 ;; Local Variables:
 ;; coding: utf-8
